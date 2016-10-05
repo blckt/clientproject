0affd10affc8de2fb573fce691b6d6977c66fc04
blckt <denis.sadilo@nixsolutions.com>
2016-10-05 14:00:27 +0300

    changes

--- lib/components/pages/ControlPanel/UserView/TableView.jsx
+++ lib/components/pages/ControlPanel/UserView/TableView.jsx
@@ -1,1 +1,27 @@
-﻿import React from 'react'
+﻿import React, { PropTypes } from 'react';
+import {
+  Table, Column, Cell
+}
+from 'fixed-data-table';
+
+class TableView extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static propTypes = {
+    data:PropTypes.arrayOf(PropTypes.any).isRequired
+  }
+
+  render() {
+    const { data } = this.props;
+    return (<Table>
+      <Column height={350}
+        width={452}
+        rowHeight={50}
+        headerHeight={50}
+        rowsCount={data.length}/>
+    </Table>);
+  }
+}
+export default TableView;
