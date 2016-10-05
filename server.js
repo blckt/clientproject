fa966e466bca478bdeca1954211812017b6f1d3d
blckt <denis.sadilo@nixsolutions.com>
2016-10-05 12:36:08 +0300

    initial

---
+++ server.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var express = require('express');
+var webpack = require('webpack');
+var config = require('./webpack.config');
+
+var app = express();
+var compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: false,
+  publicPath: config.output.publicPath,
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000 // is this the same as specifying --watch-poll?
+  }
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('*', function (req, res) {
+  console.log(req.url);
+  res.sendFile(path.join(__dirname, './dist/index.html'));
+});
+
+app.listen(3001, 'localhost', function (err) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log('Listening at http://localhost:3001');
+});
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'babel-polyfill',
+    'webpack-hot-middleware/client',
+    './lib/index'
+  ],
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/assets/'
+  },
+  resolve: {
+    alias: {
+      __DEV_URL__: 'http://localhost:3000/'
+    }
+  },
+  externals: {
+    "jQuery": "jQuery"
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      },
+      '__DEV_URL__': 'http://localhost:3000/',
+      '__DEVTOOLS__': process.env.DEVTOOLS === 'true' ? true : false
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Redux React Router Async Example',
+      filename: 'index.html',
+      template: 'index.template.html',
+      favicon: path.join(__dirname, 'assets', 'images', 'favicon.ico')
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+    })
+  ],
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style-loader!css-loader!cssnext-loader' },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        include: path.join(__dirname, 'lib'),
+        query: {
+          presets: ['react']
+        }
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+      { test: /\.(gif|jpeg)$/, loader: 'file-loader' },
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      }
+    ]
+  },
+  cssnext: {
+    browsers: 'last 2 versions'
+  }
+};
